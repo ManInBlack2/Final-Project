b72d3ccf7f119a3b2538a31c2c20ff581689b251
Liam M <marzialiliam@gmail.com>
2025-12-13 18:38:24 -0500

    Add methods to Department.

--- src/main/java/org/Liam/Department.java
+++ src/main/java/org/Liam/Department.java
@@ -1,4 +1,35 @@
 package org.Liam;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Department {
+    private String departmentId;
+    @Setter private String departmentName;
+
+    private static int nextId = 0;
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%d", nextId++);
+            this.departmentName = departmentName;
+        }
+        this.departmentId = null;
+        this.departmentName = null;
+    }
+
+    static boolean isDepartmentNameValid(String departmentName) {
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) || c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
