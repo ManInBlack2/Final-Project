205eb7d4f61473f66354de02bb750c1e62a2b451
Liam M <marzialiliam@gmail.com>
2025-12-13 13:51:18 -0500

    Add methods in Address class.

--- src/main/java/org/Liam/Address.java
+++ src/main/java/org/Liam/Address.java
@@ -1,10 +1,70 @@
 package org.Liam;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Address {
-    private int streetNo;
-    private String street;
-    private String city;
-    private Province province;
+    @Setter private Integer streetNo;
+    @Setter private String street;
+    @Setter private String city;
+    @Setter private Province province;
     private String postalCode;
 
+    public Address(Integer streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+        }
+        this.streetNo = null;
+        this.street = null;
+        this.city = null;
+        this.province = null;
+        this.postalCode = null;
+    }
+
+    public enum Province {
+        AB,
+        BC,
+        MB,
+        NB,
+        NL,
+        NS,
+        ON,
+        PE,
+        QC,
+        SK,
+        YT
+    }
+
+    static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode.length() != 6) {
+            return false;
+        }
+
+        for (char i = 0; i < postalCode.length(); i++) {
+            if (i % 2 == 0) {
+                if (!Character.isLetter(postalCode.charAt(i))) {
+                    return false;
+                }
+            }else if (!Character.isDigit(postalCode.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void setPostalCode(String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.postalCode = postalCode;
+        }
+    }
 }
