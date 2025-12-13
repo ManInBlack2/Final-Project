563247d6b696414321d0080b4a8012adad1f7a1e
Liam M <marzialiliam@gmail.com>
2025-12-13 18:44:36 -0500

    Add fields to Student.

--- src/main/java/org/Liam/Student.java
+++ src/main/java/org/Liam/Student.java
@@ -1,4 +1,15 @@
 package org.Liam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 0;
 }
