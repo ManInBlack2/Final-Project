5b369f3d4f42005af68bc935d18cd2b54a801554
Liam M <marzialiliam@gmail.com>
2025-12-13 20:55:19 -0500

    Add toTitleCase to Util.

--- src/main/java/org/Liam/Util.java
+++ src/main/java/org/Liam/Util.java
@@ -1,4 +1,19 @@
 package org.Liam;
 
 public class Util {
+    public static String toTitleCase(String str) {
+        String[] words = str.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
+        }
+
+        String str2 = words[0];
+
+        for (int i = 1; i < words.length; i++) {
+            str2 += " " + words[i];
+        }
+
+        return str2;
+    }
 }
