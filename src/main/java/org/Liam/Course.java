941fadeb9caa723a9f661bd6ccf8edde4589bbd1
Liam M <marzialiliam@gmail.com>
2025-12-13 20:30:10 -0500

    Add fields to Course.

--- src/main/java/org/Liam/Course.java
+++ src/main/java/org/Liam/Course.java
@@ -1,4 +1,14 @@
 package org.Liam;
 
+import java.util.ArrayList;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> students;
+
+    private static int nextId = 1;
 }
