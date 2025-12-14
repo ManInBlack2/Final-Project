ffa10088db10d53b7f217b67dbe772d8c0602686
Liam M <marzialiliam@gmail.com>
2025-12-14 13:33:38 -0500

    Add generateRandomScore to Assignment.

--- src/main/java/org/Liam/Assignment.java
+++ src/main/java/org/Liam/Assignment.java
@@ -3,6 +3,7 @@ package org.Liam;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Getter
 public class Assignment {
@@ -21,6 +22,22 @@ public class Assignment {
     }
 
     private void generateRandomScore() {
+        Random random = new Random();
+        for (int i = 0; i < scores.toArray().length; i++) {
+            int num1 = random.nextInt(0, 11);
+
+            if (num1 == 0) {
+                scores.toArray()[i] = random.nextInt(0, 60);
+            } else if (num1 == 1 || num1 == 2) {
+                scores.toArray()[i] = random.nextInt(60, 70);
+            } else if (num1 == 3 || num1 == 4) {
+                scores.toArray()[i] = random.nextInt(70, 80);
+            } else if (num1 == 5 || num1 == 6 || num1 == 7 || num1 == 8) {
+                scores.toArray()[i] = random.nextInt(80, 90);
+            } else if (num1 == 9 || num1 == 10) {
+                scores.toArray()[i] = random.nextInt(90, 101);
+            }
+        }
 
     }
 
