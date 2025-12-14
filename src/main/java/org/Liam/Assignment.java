86c1e0298c9fefd5a3fd06d94dba3467a730f0a7
Liam M <marzialiliam@gmail.com>
2025-12-13 20:26:39 -0500

    Add fields to Assignment.

--- src/main/java/org/Liam/Assignment.java
+++ src/main/java/org/Liam/Assignment.java
@@ -1,4 +1,10 @@
 package org.Liam;
 
+import java.util.ArrayList;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
 }
