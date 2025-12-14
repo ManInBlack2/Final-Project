37a2e9e683fb9a86b0e3e8d291d12f79e2032866
Liam M <marzialiliam@gmail.com>
2025-12-14 12:28:12 -0500

    Add methods to Student.

--- src/main/java/org/Liam/Student.java
+++ src/main/java/org/Liam/Student.java
@@ -1,8 +1,15 @@
 package org.Liam;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Student {
     private String studentId;
     private String studentName;
@@ -11,5 +18,89 @@ public class Student {
     private Department department;
     private ArrayList<Course> registeredCourses;
 
-    private static int nextId = 0;
+    private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    public boolean registerCourse(Course course) {
+        for (Course course1 : this.registeredCourses) {
+            if (course.equals(course1)) {
+                return false;
+            }
+        }
+
+        registeredCourses.add(course);
+        course.getStudents().add(Student.this);
+
+        for (Assignment assignment : course.getAssignments()) {
+            assignment.getScores().add(null);
+        }
+
+        return true;
+    }
+
+    public boolean dropCourse(Course course) {
+        boolean hasCourse = false;
+        for (Course course1 : this.registeredCourses) {
+            if (course.equals(course1)){
+                hasCourse = true;
+                break;
+            }
+        }
+
+        if (hasCourse) {
+            for (int i = 0; i < registeredCourses.toArray().length; i++) {
+                if (registeredCourses.get(i) == course) {
+                    registeredCourses.remove(i);
+                    break;
+                }
+            }
+            for (int i = 0; i < course.getStudents().toArray().length; i++) {
+                if (course.getStudents().get(i) == Student.this) {
+                    course.getStudents().remove(i);
+                    break;
+                }
+            }
+        }
+
+        return hasCourse;
+    }
+
+//    public static String toSimplifiedString(ArrayList<Student> students) {
+//        String students1;
+//        for (Student student : students) {
+//
+//        }
+//
+//
+//
+//
+//
+//        return ;
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "studentId='" + studentId + '\'' +
+//                ", studentName='" + studentName + '\'' +
+//                ", gender=" + gender +
+//                ", address=" + address +
+//                ", department=" + department +
+//                ", registeredCourses=" + registeredCourses +
+//                '}';
+//    }
+
 }
