10211695faf71153fc9c9b8fa15c064765c3f297
Liam M <marzialiliam@gmail.com>
2025-12-14 22:13:11 -0500

    Fix generateRandomScore in Assignment.

--- src/main/java/org/Liam/Assignment.java
+++ src/main/java/org/Liam/Assignment.java
@@ -36,19 +36,20 @@ public class Assignment {
 
     public void generateRandomScore() {
         Random random = new Random();
+
         for (int i = 0; i < scores.toArray().length; i++) {
             int num1 = random.nextInt(0, 11);
 
             if (num1 == 0) {
-                scores.toArray()[i] = random.nextInt(0, 60);
+                scores.set(i, random.nextInt(0, 60));
             } else if (num1 == 1 || num1 == 2) {
-                scores.toArray()[i] = random.nextInt(60, 70);
+                scores.set(i, random.nextInt(60, 70));
             } else if (num1 == 3 || num1 == 4) {
-                scores.toArray()[i] = random.nextInt(70, 80);
+                scores.set(i, random.nextInt(70, 80));
             } else if (num1 == 5 || num1 == 6 || num1 == 7 || num1 == 8) {
-                scores.toArray()[i] = random.nextInt(80, 90);
-            } else if (num1 == 9 || num1 == 10) {
-                scores.toArray()[i] = random.nextInt(90, 101);
+                scores.set(i, random.nextInt(80, 90));
+            } else {
+                scores.set(i, random.nextInt(90, 101));
             }
         }
 
