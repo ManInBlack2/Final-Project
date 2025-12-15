83af4aa2c58eeab2d716db155353ac38f48e9f07
Liam M <marzialiliam@gmail.com>
2025-12-14 19:14:56 -0500

    Add methods to Course.

--- src/main/java/org/Liam/Course.java
+++ src/main/java/org/Liam/Course.java
@@ -1,7 +1,14 @@
 package org.Liam;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Course {
     private String courseId;
     private String courseName;
@@ -11,4 +18,102 @@ public class Course {
     private ArrayList<Student> students;
 
     private static int nextId = 1;
+
+    public Course(String courseName, double credits, Department department) {
+        this.courseId = String.format("C-%s-%02d", department.getDepartmentId(), nextId++);
+        this.courseName = courseName;
+        this.credits = credits;
+        this.department = department;
+        this.assignments = null;
+        this.students = null;
+    }
+
+    public boolean isAssignmentWeightValid() {
+        double sum = 0;
+
+        for (Assignment assignment : assignments) {
+            sum += assignment.getWeight();
+        }
+
+        return sum == 100;
+    }
+
+    public boolean registerStudent(Student student) {
+        students.add(student);
+
+        for (Assignment assignment : assignments) {
+            assignment.getScores().add(null);
+        }
+
+        return true;
+    }
+
+    public int[] calcStudentsAverage() {
+        int[] averages = new int[students.toArray().length];
+
+        for (int i = 0; i < students.toArray().length; i++) {
+            for (Assignment assignment : assignments) {
+                averages[i] += (int) (assignment.getScores().toArray().length * (assignment.getWeight() / 100));
+            }
+        }
+
+        return averages;
+    }
+
+    public boolean addAssignment(String assignmentName, double weight) {
+        Assignment assignment = new Assignment(assignmentName, weight);
+
+        for (Student student : students) {
+            assignment.getScores().add(null);
+        }
+
+        assignments.add(assignment);
+        return true;
+    }
+
+    public Integer[] generateScores() {
+        for (int i = 0; i < assignments.toArray().length; i++) {
+            assignments.get(i).generateRandomScore();
+        }
+
+        Integer[] sums = new Integer[students.toArray().length];
+
+        for (int i = 0; i < assignments.toArray().length; i++) {
+            for (int j = 0; j < sums.length; j++) {
+                sums[j] += (int)(assignments.get(i).getScores().get(j) * assignments.get(i).getWeight() / 100);
+            }
+        }
+
+        return sums;
+    }
+
+    public void displayScores() {
+        Integer[] avgs = generateScores();
+
+        System.out.printf("Course: %s(%s)\n", courseName, courseId);
+        System.out.print("                           ");
+        for (int i = 0; i < assignments.toArray().length; i++) {
+            System.out.printf("%15s", assignments.get(i).getAssignmentName());
+        }
+        System.out.print("    Final Score\n");
+        for (int i = 0; i < students.toArray().length; i++) {
+            System.out.printf("        %-20s", students.get(i).getStudentName());
+            for (Assignment assignment : assignments) {
+                System.out.printf("%15d", assignment.getScores().get(i));
+            }
+            System.out.printf("%15d\n", avgs[i]);
+        }
+        System.out.println("        Average             ");
+        for (Assignment assignment : assignments) {
+            System.out.printf("%15f", assignment.calcAssignmentAvg());
+        }
+    }
+
+//    public String toSimplifiedString() {
+//        return "courseId=" + courseId +
+//                "courseName=" + courseName +
+//                "credits=" + credits +
+//                "departmentName" + department;
+//    }
+
 }
